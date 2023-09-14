4a234a916ff7fabbd20d53e6cab95bd164e38101
Guilherme  Santos <guilherme.santos@quaestum.com.br>
2023-09-14 10:01:39 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSoundAPI/Program.cs
@@ -0,0 +1,17 @@
+﻿using ScreenSoundAPI.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resp = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");       
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resp)!;
+        musicas[0].ExibirDetalhesDaMusica();
+    }
+    catch  (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema:{ex.Message} ");
+    }
+   
+}    
